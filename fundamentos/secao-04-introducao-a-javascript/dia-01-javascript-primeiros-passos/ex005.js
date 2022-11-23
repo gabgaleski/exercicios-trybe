873a55a58dcbb593a9aacaab1154864a73c2a650
gabgaleski <galeski35@gmail.com>
2022-11-23 16:06:17 -0300

    Cria arquivo js com testes de switch/case

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/ex005.js
@@ -0,0 +1,18 @@
+let resultado ="Aprovada"
+
+switch (resultado) {
+    case "Aprovada":
+        console.log("Parabéns, você foi aprovada(o)");
+    break;
+
+    case "lista":
+        console.log("Você esta na lista de espera")
+    break;
+
+    case "Reprovada":
+        console.log("Você foi reprovada(o)")
+    break;
+    
+    default:
+        console.log("Informação incorreta")
+}
