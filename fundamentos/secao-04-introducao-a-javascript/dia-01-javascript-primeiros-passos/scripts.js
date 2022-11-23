7ccb764d8fc88b6b67f951b0191bff92144dd36d
gabgaleski <galeski35@gmail.com>
2022-11-23 13:46:23 -0300

    Adiciona 3 variaveis e 3 console.log nela. Testando variaveis

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,1 +1,8 @@
-let age = 20;
+const myName = "Gabriel"
+const birthCity = "Cascavel"
+const birthYear = 1998
+birthYear = 2030
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
