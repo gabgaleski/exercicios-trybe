36501b4d106810fca128aab979905b3e1633107c
gabgaleski <galeski35@gmail.com>
2022-11-24 18:56:16 -0300

    Testes com if/else

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/ex007.js
@@ -0,0 +1,9 @@
+const numero1 = 67;
+const numero2 = 60;
+if (numero1 > numero2) {
+    console.log(`O numero ${numero1} é o maior!`)
+} else if (numero1 === numero2) {
+    console.log("Os numeros sao iguais!")
+} else {
+    console.log(`O numero ${numero2} é o maior`)
+}
