7c0121583772a48f0162f706a52d9b7af561e4a8
gabgaleski <galeski35@gmail.com>
2022-12-12 22:19:48 -0300

    Resolve parte 1

--- fundamentos/secao-05-js-dom-eventos-webstorage/dia-03-eventos/calendario/script.js
+++ fundamentos/secao-05-js-dom-eventos-webstorage/dia-03-eventos/calendario/script.js
@@ -15,3 +15,25 @@ const createDaysOfTheWeek = () => {
   
   // Escreva seu código abaixo.
   
+    const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+    const days = document.getElementById("days");
+
+ 
+      for (let index = 0; index < decemberDaysList.length; index += 1) {
+        const numberDay = document.createElement("li");
+        numberDay.className = "day";
+        numberDay.innerHTML = decemberDaysList[index];
+        days.appendChild(numberDay)
+        const dayIndex = decemberDaysList[index];
+
+        if (dayIndex === 24 || dayIndex === 25 || dayIndex === 31 ) {
+          numberDay.className = "day holiday";
+        } 
+        if (dayIndex === 4 || dayIndex === 11 || dayIndex === 18 || dayIndex === 25) {
+            numberDay.className = "day friday";
+
+        }
+      }
+  
+
