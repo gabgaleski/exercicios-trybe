71cff63cf25e54a344a965572568839f1e433cd6
gabgaleski <galeski35@gmail.com>
2022-12-12 22:40:37 -0300

    Realiza parte 2 do exercicio

--- fundamentos/secao-05-js-dom-eventos-webstorage/dia-03-eventos/calendario/script.js
+++ fundamentos/secao-05-js-dom-eventos-webstorage/dia-03-eventos/calendario/script.js
@@ -36,4 +36,15 @@ const createDaysOfTheWeek = () => {
         }
       }
   
-
+      const createHolidayButton = (buttonName) => {
+        let buttonContainer = document.querySelector('.buttons-container');
+        let newButton = document.createElement('button');
+        let newButtonID = 'btn-holiday';
+      
+        newButton.innerHTML = buttonName;
+        newButton.id = newButtonID;
+        
+        buttonContainer.appendChild(newButton);
+      }
+      
+      createHolidayButton('Feriados');
