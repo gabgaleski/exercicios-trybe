45e840ad51fa4e51040eb89cac78d02e2e792cf7
gabgaleski <galeski35@gmail.com>
2022-12-13 19:18:53 -0300

    Resolve 2a questao

--- fundamentos/secao-05-js-dom-eventos-webstorage/dia-04-web-storage/js001.js
+++ fundamentos/secao-05-js-dom-eventos-webstorage/dia-04-web-storage/js001.js
@@ -1,6 +1,6 @@
 const backGround = document.getElementById('background-color');
 const colorBtn = document.getElementsByClassName('colorBtn');
-
+const fontColor = document.getElementsByClassName('fontColor');
 
 const setBackground = (color) => {
 let content = document.getElementsByClassName("body")[0];
@@ -15,3 +15,17 @@ for (let index = 0; index < colorBtn.length; index += 1) {
 }
 
 ///////////////////////////////////////////////////////////////
+
+const setFont = (color) => {
+    let text = document.getElementsByClassName('paragraph');
+    for (let index = 0; index < text.length; index += 1){
+        text[index].style.color = color;
+    }
+    localStorage.setItem('fontColor', color);
+}
+
+for (let index1 = 0; index1 < fontColor.length; index1 += 1) {
+    fontColor[index1].addEventListener('click', (event) => {
+        setFont(event.target.innerHTML);
+    })
+}
