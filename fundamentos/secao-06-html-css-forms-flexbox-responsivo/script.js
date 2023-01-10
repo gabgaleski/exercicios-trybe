7944d00b1d8a0dcb784dc560a463ce91047af60a
gabgaleski <galeski35@gmail.com>
2023-01-10 17:55:08 -0300

    realiza um formulario com html css e js

---
+++ fundamentos/secao-06-html-css-forms-flexbox-responsivo/script.js
@@ -0,0 +1,6 @@
+const getBtn = document.querySelector('#submit-button');
+const termos = document.querySelector('#termos');
+
+getBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+});
