c3019d872eacfad1c33fcf1903f184033db6e805
gabgaleski <galeski35@gmail.com>
2023-01-10 18:23:53 -0300

    Ao confirmar os termos de imagem abre um campo para upar arquivos

--- fundamentos/secao-06-html-css-forms-flexbox-responsivo/script.js
+++ fundamentos/secao-06-html-css-forms-flexbox-responsivo/script.js
@@ -1,6 +1,28 @@
 const getBtn = document.querySelector('#submit-button');
 const termos = document.querySelector('#termos');
+const divTermos = document.querySelector('#div-termos')
 
 getBtn.addEventListener('click', (event) => {
   event.preventDefault();
 });
+
+termos.addEventListener('click', () => {
+  if (termos.className == 'on') {
+    termos.classList = 'off';
+  } else {
+    termos.classList = 'on';
+  }
+  createFile();
+});
+
+const createFile = () => {
+    if (termos.classList == 'on') {
+        const generateFile = document.createElement('input');
+        generateFile.type = 'file';
+        generateFile.id = 'file'
+        divTermos.appendChild(generateFile);
+    } else {
+      const deletFile = document.querySelector('#file');
+      deletFile.remove();
+    }
+};
